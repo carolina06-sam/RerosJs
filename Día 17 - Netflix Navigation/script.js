3583226f6db314ab4cef992c26e22464a7015b14
Carolina Samaca <carolinasamacagonzalez@gmail.com>
2021-09-18 18:37:52 -0500

    retos

--- Día 17 - Netflix Navigation/script.js
+++ Día 17 - Netflix Navigation/script.js
@@ -1,11 +1,10 @@
-const open_btn = document.querySelector('.open-btn')
-const close_btn = document.querySelector('.close-btn')
-const nav = document.querySelectorAll('.nav')
+const open_btn = document.querySelector(".open-btn");
+const close_btn = document.querySelector(".close-btn");
+const nav = document.querySelectorAll(".nav");
 
-open_btn.addEventListener('click', () => {
-    nav.forEach(nav_el => nav_el.classList.add('visible'))
-})
-
-close_btn.addEventListener('click', () => {
-    nav.forEach(nav_el => nav_el.classList.remove('visible'))
-})
+open_btn.addEventListener("click", () => {
+    nav.forEach((nav_element) => nav_element.classList.add("visible"));
+});
+close_btn.addEventListener("click", () => {
+    nav.forEach((nav_element) => nav_element.classList.remove("visible"));
+});
