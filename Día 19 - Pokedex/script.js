e8373eebeac437b4cf4d4560c92b24df4e9d68f7
Carolina Samaca <carolinasamacagonzalez@gmail.com>
2021-09-21 21:08:05 -0500

    reto 19

--- Día 19 - Pokedex/script.js
+++ Día 19 - Pokedex/script.js
@@ -16,35 +16,37 @@ const colors = {
     fighting: "#E6E0D4",
     normal: "#F5F5F5",
 };
-
+//object Es es usado para guardar una colección de datos definidos
 const main_types = Object.keys(colors);
-
+// la cantidad de pokemmones que necesitamos la promesa away
 const fetchPokemons = async () => {
     for (let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i);
     }
 };
-
+// SOLICITA LOS DATOS A LA API
 const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const data = await res.json();
     createPokemonCard(data);
 };
-
+/* crea las tajetas para mostrar */
 const createPokemonCard = (pokemon) => {
     const pokemonEl = document.createElement("div");
     pokemonEl.classList.add("pokemon");
-
+    //dejar la primera letra en mayuscula
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
     const id = pokemon.id.toString().padStart(3, "0");
 
     const poke_types = pokemon.types.map((type) => type.type.name);
+    console.log(poke_types);
     const type = main_types.find((type) => poke_types.indexOf(type) > -1);
     const color = colors[type];
+    //find busca elprimer numero del array que cumpla con ka condiccion
 
     pokemonEl.style.backgroundColor = color;
-
+    // cada etiqueta
     const pokemonInnerHTML = `
     <div class="img-container">
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"" alt="${name}">
@@ -56,7 +58,7 @@ const createPokemonCard = (pokemon) => {
     </div>
     `;
 
-    pokemonEl.innerHTML = pokemonInnerHTML;
+    pokemonEl.innerHTML = pokemonInnerHTML; /* la agregamos a nustro html */
 
     poke_container.appendChild(pokemonEl);
 };
